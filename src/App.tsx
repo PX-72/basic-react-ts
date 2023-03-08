b35e3f5f96ba031fadea263202fc0da8edc3dce4
PX-72 <zoli.konyves@pm.me>
2023-03-08 20:15:07 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div>HELLO</div>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root: HTMLElement = document.querySelector('#app')!;
+ReactDOM.createRoot(root).render(<App/>);
